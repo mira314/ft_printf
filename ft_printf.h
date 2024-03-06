83af6f4a9bfde1b4341140973d417dc1a842cbbb
Valisoa Mirado Narindra Randrianoelisoa <vrandria@c1r7s12.42antananarivo.mg>
2024-03-06 10:40:41 +0300

    240306

--- ft_printf.h
+++ ft_printf.h
@@ -1,11 +1,23 @@
-#ifndef FT_PRINTER_H
-# define FT_PRINTER_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vrandria <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/06 10:37:03 by vrandria          #+#    #+#             */
+/*   Updated: 2024/03/06 10:37:06 by vrandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 # include <stdarg.h>
 # include <stdlib.h>
 # include <unistd.h>
 
-int ft_putnbr(int nb);
-int ft_putchar(char c);
-int ft_putstr(char *s);
+int	ft_putnbr(int nb);
+int	ft_putchar(char c);
+int	ft_putstr(char *s);
 
 #endif
--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,29 +6,37 @@
 /*   By: vrandria <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 10:20:28 by vrandria          #+#    #+#             */
-/*   Updated: 2024/03/06 10:20:29 by vrandria         ###   ########.fr       */
+/*   Updated: 2024/03/06 10:33:28 by vrandria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_putnbr(int nb)
+int	ft_putnbr(int nb)
 {
-    char    c;
-    int     i;
-    
-    i = 0;
-    if (nb =-2147483648)
-    {
-        write(1, "-2147483648", 11)
-        return (11);
-    }
-    if (nb < 0)
-    {
-        i += ft_putchar('-');
-        i += ft_putnbr(-nb);
-        
-    }
+	char	c;
+	int		i;
+
+	i = 0;
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (11);
+	}
+	if (nb < 0)
+	{
+		i += ft_putchar('-');
+		i += ft_putnbr(-nb);
+	}
+	if (nb > 9)
+	{
+		i += ft_putnbr(nb / 10);
+		i += ft_putnbr(nb % 10);
+	}
+	if (nb >= 0 && nb < 10)
+	{
+		c = nb + 48;
+		i += ft_putchar(c);
+	}
+	return (i);
 }
-
-
